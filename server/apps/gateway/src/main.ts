5b4965b54d2e0a63dcfcd7e984c11bbacfb51787
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2024-02-26 09:37:24 +0800

    server

--- server/apps/gateway/src/main.ts
+++ server/apps/gateway/src/main.ts
@@ -5,5 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(5000);
+  console.log("Server is running on port 5000");
 }
 bootstrap();

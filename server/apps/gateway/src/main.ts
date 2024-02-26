6f27d3fa1edb4dae4caa184155c1274a2b1dc46d
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2024-02-26 11:22:05 +0800

    change server port

--- server/apps/gateway/src/main.ts
+++ server/apps/gateway/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(8081);
-  console.log('Server is running on port 8081');
+  await app.listen(5001);
+  console.log('Server is running on port 5001');
 }
 bootstrap();
